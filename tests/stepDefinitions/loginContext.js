01948d179c812f10d3fb056612b9cba38ee6b1c2
Nitesh raj baral <nbaral640@gmail.com>
2024-12-17 15:35:27 +0545

    test:implement pageObject model
    Signed-off-by: Nitesh raj baral <nbaral640@gmail.com>

--- tests/stepDefinitions/loginContext.js
+++ tests/stepDefinitions/loginContext.js
@@ -1,40 +1,31 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
 const { expect } = require("@playwright/test");
-
-const loginBtnSelector = ".btn.btn-block.btn-primary"
-const emailFieldSelector = "#spree_user_email"
-const passwordFieldSelector = "#spree_user_password"
-const dashboardSelector = "//div[@id='missingHomepage']//h1"
-const invalidErrorSelector = ".alert-danger"
+const { LoginPage } = require("../pageObjects/LoginPage");
+const loginPage = new LoginPage();
 
 Given('user {string} has navigated to the admin login page', async function (user) {
-  await page.goto("http://127.0.0.1:3000/admin/login")
+  await loginPage.navigateToAdminLoginPage()
   await expect(page).toHaveURL("http://127.0.0.1:3000/admin/login")
 });
 
 When('user {string} logs in with following credentials',async function (user, credentials) {
   for (const { email, password } of credentials.hashes()) {
-    await page.fill(emailFieldSelector, email);
-    await page.fill(passwordFieldSelector, password);
-    await page.click(loginBtnSelector);
+    await loginPage.login(email,password)
   }
 });
 
 Then('user {string} should be navigated to admin panel dashboard', async function (user) {
   await page.waitForURL("http://127.0.0.1:3000/", { timeout: 5000 });
   await expect(page).toHaveURL("http://127.0.0.1:3000/")
-  await expect(page.locator(dashboardSelector)).toBeVisible();
+  await expect(page.locator(loginPage.dashboardSelector)).toBeVisible();
 });
 
 When('the user tries to log in with following credentials',async function (credentials) {
   for (const { email, password } of credentials.hashes()) {
-    await page.fill(emailFieldSelector, email);
-    await page.fill(passwordFieldSelector, password);
-    await page.click(loginBtnSelector);
+    await loginPage.login(email,password)
   }
 });
 
 Then('error message {string} should be shown',async function (errorMessage) {
-  await expect(page.locator(invalidErrorSelector)).toHaveText(errorMessage);
-
+  await expect(page.locator(loginPage.errorMessageSelector)).toHaveText(errorMessage);
 });
